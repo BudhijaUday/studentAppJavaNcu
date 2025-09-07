446e1cb01e8579278cf0eb5e713301684fd08334
Uday Budhija <udaybudhija26@gmail.com>
2025-09-07 17:53:02 +0530

    done some new work

--- stuapp/src/main/java/com/mystudent/StudentServiceApplication.java
+++ stuapp/src/main/java/com/mystudent/StudentServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.school")
+@ComponentScan(basePackages = "com.mystudent")
 public class StudentServiceApplication 
 {
     public static void main(String[] args) 
